d44030fcd3c584e05e8fcc321e510da660a4a0e3
Diana <dianavodopivec16@gmail.com>
2025-05-28 01:54:49 -0300

    feat: remove orbitcontrols

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,12 +1,10 @@
 import { Canvas } from "@react-three/fiber";
 import { Frames } from "./Frames";
-import { OrbitControls } from "@react-three/drei";
 
 export const Gallery = () => {
   return (
     <Canvas camera={{ position: [0, 10, 25], fov: 28 }} flat>
       <Frames />
-      {/* <OrbitControls /> */}
     </Canvas>
   );
 };
