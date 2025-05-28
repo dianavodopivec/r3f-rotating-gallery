dcc337ef0aca589c60f068256d266c2b7ffd05b5
Diana <dianavodopivec16@gmail.com>
2025-05-28 01:50:28 -0300

    feat: generate multiples frames from base texture + geometry

--- src/components/Frames.tsx
+++ src/components/Frames.tsx
@@ -1,11 +1,15 @@
 import { textures } from "../data/textures"
 import { Geometry } from "./Geometry"
 
+const mappingFrames = (number: number) => Array(number).fill(textures).flat()
+
 export const Frames = () => {
+  const newFrames = mappingFrames(5)
+
   return (
     <>
-      {textures.map((frame, i) => (
-        <Geometry key={i} frame={frame} position={[i, 0, 0]}/>
+      {newFrames.map((frame, i) => (
+        <Geometry key={i} frame={frame} index={i} total={newFrames.length}/>
       ))}
     </>
   )
