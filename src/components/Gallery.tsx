92737ceb54e63475067656f2063c0e1eb985b46a
Diana <dianavodopivec16@gmail.com>
2025-05-28 01:48:01 -0300

    feat: add camera position + fov

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,12 +1,12 @@
-import { Canvas } from "@react-three/fiber"
-import { Frames } from "./Frames"
-import { OrbitControls } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber";
+import { Frames } from "./Frames";
+import { OrbitControls } from "@react-three/drei";
 
 export const Gallery = () => {
   return (
-    <Canvas>
-      <Frames/>
-      <OrbitControls/>
+    <Canvas camera={{ position: [0, 10, 25], fov: 28 }} flat>
+      <Frames />
+      {/* <OrbitControls /> */}
     </Canvas>
-  )
-}
+  );
+};
